43422d117d89a7c1816795c2e3085bb7a055b546
FizanielX <fizanieru@gmail.com>
2026-03-15 07:34:53 +0700

    Update bot and owner details in settings

--- setting.js
+++ setting.js
@@ -1,13 +1,13 @@
 //=====💫 sᴛᴀʀ  xᴅ🔥====
 
 module.exports = {
-  BOT_NAME: 'sᴛᴀʀ xᴅ', // ur bot name
-  OWNER_NAME: 'ᴍᴀʟᴠɪɴ ᴋɪɴɢ', //your name
-  OWNER_NUMBER: '263714757857', // add your phone number
+  BOT_NAME: 'Reivery', // ur bot name
+  OWNER_NAME: 'Fizanieru', //your name
+  OWNER_NUMBER: '6282143392740', // add your phone number
   PREFIXES: ['.', '!', '#'], // Multiple prefix support you can add one or more
   
   
-  SESSION_ID: '', // add your session ID
-  PAIRING_NUMBER: '', // In Case if you don't have session id get pairing code by putting your number here
-  TIMEZONE: 'Africa/Harare' // ur timezone
+  SESSION_ID: 'king~P2beh4', // add your session ID
+  PAIRING_NUMBER: '6285281876697', // In Case if you don't have session id get pairing code by putting your number here
+  TIMEZONE: 'Indonesia/Jakarta' // ur timezone
 };
